80307e6793b1e876bf1c1e4e17a239980bb25819
Tung N Le <tung.le0319@gmail.com>
2022-12-04 22:48:31 -0700

    tung

--- components/CreatePost.js
+++ components/CreatePost.js
@@ -3,7 +3,6 @@ import React from "react";
 export default function CreatePost(){
   return(
 <div className="container  border-red-300 border shadow-2xl flex justify-center">
-<p className="text-3xl py-2 "> Create A Post</p>
 
 </div>
 )  
